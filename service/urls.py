7b44d63667b6993bdd4b1ea06f4e71d78ecd7ed3
Ihor Ivashov <dm-i@i.ua>
2023-03-15 16:55:23 +0200

    Add part views and templates - list, detail, create, delete

--- service/urls.py
+++ service/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from service.views import (
     index, CarListView, CarDetailView, CarCreateView, CarUpdateView,
-    CarDeleteView
+    CarDeleteView, PartListView, PartDetailView, PartCreateView,
+    PartUpdateView, PartDeleteView
 )
 
 urlpatterns = [
@@ -12,7 +13,19 @@ urlpatterns = [
     path("cars/create", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
-
+    path("parts/", PartListView.as_view(), name="part-list"),
+    path("parts/<int:pk>/", PartDetailView.as_view(), name="part-detail"),
+    path("parts/create", PartCreateView.as_view(), name="part-create"),
+    path(
+        "parts/<int:pk>/update/",
+        PartUpdateView.as_view(),
+        name="part-update"
+    ),
+    path(
+        "parts/<int:pk>/delete/",
+        PartDeleteView.as_view(),
+        name="part-delete"
+    ),
 ]
 
 app_name = "service"
--- service/views.py
+++ service/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from service.models import Customer, Car, Order
+from service.models import Customer, Car, Order, Part
 
 
 def index(request):
@@ -26,7 +26,7 @@ def index(request):
 
 class CarListView(LoginRequiredMixin, generic.ListView):
     model = Car
-    paginate_by = 5
+    paginate_by = 12
     queryset = Car.objects.all().prefetch_related("customers")
 
 
@@ -47,3 +47,29 @@ class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
 class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Car
     success_url = reverse_lazy("service:car-list")
+
+
+class PartListView(LoginRequiredMixin, generic.ListView):
+    model = Part
+    paginate_by = 12
+
+
+class PartDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Part
+
+
+class PartCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Part
+    fields = "__all__"
+    success_url = reverse_lazy("service:part-list")
+
+
+class PartUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Part
+    fields = "__all__"
+    success_url = reverse_lazy("service:part-list")
+
+
+class PartDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Part
+    success_url = reverse_lazy("service:part-list")
