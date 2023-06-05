a547b32241715065bb963015c68dca5982a54c94
Ihor Ivashov <dm-i@i.ua>
2023-06-05 11:41:12 +0300

    Add car-service-f92j.onrender.com to ALLOWED_HOSTS

--- car_service/settings.py
+++ car_service/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "car-service-f92j.onrender.com"]
 
 
 # Application definition
